0e516a44d3c30a3b9860242dc2724d057be30358
Vlad <vladyslav.dokukin@gmail.com>
2022-07-10 09:15:48 +0300

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-let number = 5;
-alert(number);
+const numberOfMovies = +prompt("Сколько фильмов вы уже просмотрели?", "");
+const personalMovieDB = {
+    count: numberOfMovies,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    private: false,
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = +prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a]= b;
+personalMovieDB.movies[c]= d;
+
+console.log(personalMovieDB);
+//создать коммит и запушить на ГитХаб
